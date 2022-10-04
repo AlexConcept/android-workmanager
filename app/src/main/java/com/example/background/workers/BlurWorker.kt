7f344d4f741ac638993036883140c38c2b5b006f
Oleksandr <alex505osip@gmail.com>
2022-10-04 16:15:28 +0300

    Implement input and output data objects

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -4,10 +4,13 @@ import android.app.Application
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.nfc.Tag
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
 import com.example.background.R
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
@@ -15,18 +18,20 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
         val appContext = applicationContext
         makeStatusNotification("Blurring image", appContext)
 
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
         return try {
-            val picture = BitmapFactory.decodeResource(
-                appContext.resources,
-                R.drawable.android_cupcake
-            )
+           val picture = BitmapFactory.decodeStream(appContext.contentResolver.openInputStream(Uri.parse(resourceUri)))
+
             val output = blurBitmap(picture, appContext)
 
             val outputUri = writeBitmapToFile(appContext, output)
 
             makeStatusNotification("Output is $outputUri", appContext)
 
-            Result.success()
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+            Result.success(outputData)
         } catch (throwable: Throwable) {
             Log.e(TAG, "Error applying blur")
             Result.failure()
